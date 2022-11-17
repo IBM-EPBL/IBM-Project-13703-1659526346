7ea1ccb52463222fc8aeb28c07ddaadf24f35f10
Dharu10 <113500792+Dharu10@users.noreply.github.com>
2022-11-17 15:09:08 +0530

    Add files via upload

---
+++ Pre-Development/Project Development Phase/Sprint1-20221117T091104Z-001/Sprint2-20221117T091105Z-001/Sprint2/static/script/script.js
@@ -0,0 +1,11 @@
+function showIncome() {
+    var incomeAdded = parseInt(document.getElementById('addIncome').value);
+
+    return  document.getElementById('incomeAdded').innerHTML = document.getElementById('addIncome').value;
+}
+
+function getData() {
+    if (input > 10) {
+        alert('Danger');
+    }
+}
